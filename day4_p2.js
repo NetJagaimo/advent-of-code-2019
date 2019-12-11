55c117bbf0a5ea83e29f2c9e19e1b32e3d2860b3
Sam Yang <samabc75@gmail.com>
2019-12-12 00:27:57 +0800

    Add some comment

--- day4_p2.js
+++ day4_p2.js
@@ -3,6 +3,7 @@ let count = 0;
 for(let i = 172930; i <= 683082; i++){
 	let num = i.toString();
 
+	// 這邊不考慮兩個數字分開，只要有剛剛好出現兩個數字就算。因為反正如果兩數是分開的，後面的條件也不會通過。
 	let double = false;
 	for(let j = 0; j <= 9; j++){
 		let re = new RegExp(j.toString(), 'g');
@@ -23,12 +24,13 @@ for(let i = 172930; i <= 683082; i++){
 	for(let j = 0; j < num.length - 1; j++){
 		if(parseInt(num[j]) > parseInt(num[j+1])){
 			decrease = false;
+			break;
 		}
 	}
 	if(!decrease){
 		continue;
 	}
-
+	console.log(num);
 	count++;
 }
 
